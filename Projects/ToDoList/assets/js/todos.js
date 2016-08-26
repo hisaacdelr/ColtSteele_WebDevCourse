922615e5ff3eca88d617e35ed82f40af91f2bfb4
Cecille Del Rosario <cecdelr@gmail.com>
2016-08-25 22:38:13 -0700

    Update Todo List (HTML, CSS, JS)

--- Projects/ToDoList/assets/js/todos.js
+++ Projects/ToDoList/assets/js/todos.js
@@ -1,5 +1,5 @@
 //Check Off Specific Todos By Clicking
-//as opposed to $("li").click(function(){})... because that only creates
+//use .on() as opposed to $("li").click(function(){})... because that only creates
 //listeners to already existing lis and doesn't account for
 //lis created in the future
 $("ul").on("click", "li", function(){
@@ -27,16 +27,21 @@ $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
-	event.stopPropagation(); //stops event bubbling
+	event.stopPropagation(); //stops event bubbling, 
+							 //in this case, stops from triggering parent element click (the strikethrough when you click the li)
 });
 
-$("input[type=text]").keypress(function(event){
+$("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		//grabbing new todo text from input
 		var todoText = $(this).val();
 		$(this).val("");
 		//create a new li and add to ul
-		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todoText + "</li>");
 		//remove text from input
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
