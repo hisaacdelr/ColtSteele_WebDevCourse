67c4a556bb393fb2e01f595b20539f1589813179
Cecille Del Rosario <cecdelr@gmail.com>
2016-08-28 13:23:57 -0700

    added comments

--- Projects/ToDoList/assets/js/todos.js
+++ Projects/ToDoList/assets/js/todos.js
@@ -21,7 +21,7 @@ $("ul").on("click", "li", function(){
 
 });
 
-//click on X to delete Todo
+//click on Trash Can to delete Todo
 //event bubbling, event goes up to parent elements
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
@@ -32,16 +32,18 @@ $("ul").on("click", "span", function(event){
 });
 
 $("input[type='text']").keypress(function(event){
+	//13 is the character code for the Enter key
 	if(event.which === 13){
 		//grabbing new todo text from input
 		var todoText = $(this).val();
-		$(this).val("");
-		//create a new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todoText + "</li>");
 		//remove text from input
+		$(this).val("");
+		//create a new li for tasks and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todoText + "</li>");
 	}
 });
 
+//toggles the input box
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
 });
