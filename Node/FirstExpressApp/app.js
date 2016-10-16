6b15330d9a956c543b87d1cbd6bdc36149dd3fed
Cecille Del Rosario <cecdelr@gmail.com>
2016-10-16 16:51:23 -0700

    route parameters and catch all * route matcher lesson

--- Node/FirstExpressApp/app.js
+++ Node/FirstExpressApp/app.js
@@ -16,9 +16,23 @@ app.get("/bye", function(req, res){
 // "/dog" => "WOOF!"
 app.get("/dog", function(req,res){
 	//triggered as soon as someone makes a request to /dog from anywhere
-	console.log("Someone made a request to /dog");
+	//console.log("Someone made a request to /dog");
 
 	res.send("WOOF!");
+});
+
+app.get("/r/:subredditName", function(req, res){
+	var subreddit = req.params.subredditName; //subredditName is from the URL request after the colon
+	res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+	//req.params would now have variables for 'subredditName', 'id', and 'title'
+	res.send("Welcome to the comments page");
+});
+
+app.get("*", function(req, res){
+	res.send("You are a star!");
 });
 
 //Tell Express to listen for requests (start server)
