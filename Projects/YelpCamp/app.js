bd74f8bdf75e684d8a6867268b7f63de8b5387c7
Cecille Del Rosario <cecdelr@gmail.com>
2016-12-04 23:06:00 -0800

    Changed YelpCamp to run on MongoDB instead of an object

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -1,42 +1,57 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+//shortcut for saving multiple var declarations in a row
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var imgurl = "https://b.fastcompany.net/multisite_files/fastcompany/imagecache/inline-small/inline/2015/09/3050613-inline-i-2-googles-new-logo-copy.png";
+// SCHEMA SETUP
+//how each campground is set up
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+//set up model
+var Campground = mongoose.model("Campground", campgroundSchema);
 
-var campGrounds = [
-	{name: "Billy", image: imgurl},
-	{name: "Bob", image: imgurl},
-	{name: "My little pony", image: imgurl},
-	{name: "Shakespeare", image: imgurl},
-	{name: "SHARON", image: imgurl},
-	{name: "Carmilla", image: imgurl},
-	{name: "Laura", image: imgurl},
-	{name: "Kaitlyn", image: imgurl},
-	{name: "Perry", image: imgurl},
-	{name: "Kirsch", image: imgurl},
-	{name: "Will", image: imgurl},
-	{name: "Mattie", image: imgurl}
-
-];
+// Campground.create(
+// 	{
+// 		name: "Woodland Park Zoo", 
+// 		image: "https://s3-media1.fl.yelpcdn.com/bphoto/Eoeekwd6uKq-Jpm-_dwhEA/90s.jpg"
+// 	}, function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("Newly created campground");
+// 			console.log(campground);
+// 		}
+// 	}
+// );
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campGrounds: campGrounds});
+	//Get all campgrounds from DB
+	Campground.find({}, function(err, allcampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: allcampgrounds});
+		}
+	});
 })
 
 app.post("/campgrounds", function(req, res){
 	//get data from form and add to campground array (database later)
 
-	var name = req.body.name; //from campgrounds/new
-	var image = req.body.image;
-	campGrounds.push({name: name, image: image});
+	// var name = req.body.name; //from campgrounds/new
+	// var image = req.body.image;
+	// campGrounds.push({name: name, image: image});
 	//redirect back to campgrounds page
 	res.redirect("/campgrounds");
 });
